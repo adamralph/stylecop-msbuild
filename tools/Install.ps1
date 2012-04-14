4648e46c7ae440a45044cae429ab3cfd3902c30b
Adam Ralph <adam@adamralph.com>
2012-04-14 18:40:01 +0200

    finshed Install.ps1 added Uninstall.ps1 renamed content hook changed version to 4.7.17.1-beta1 for Nuget push added note about upgrade path

--- tools/Install.ps1
+++ tools/Install.ps1
@@ -1,10 +1,40 @@
 param($installPath, $toolsPath, $package, $project)
 
 # remove and delete content hook
-$hook = "StyleCop.MSBuild.Deleteme.txt"
-$project.ProjectItems.Item($hook).Remove();
-[System.IO.FILE]::Delete([System.IO.PATH]::COMBINE([System.IO.PATH]::GETDIRECTORYNAME($project.FullName), $hook))
+$hookName = "StyleCop.MSBuild.ContentHook.SafeToDelete.txt"
+$project.ProjectItems.Item($hookName).Remove();
+[System.IO.FILE]::Delete([System.IO.PATH]::COMBINE([System.IO.PATH]::GETDIRECTORYNAME($project.FullName), $hookName))
 
-# add import to project file
-$targetsPath = [System.IO.PATH]::COMBINE($toolsPath, "StyleCop.targets")
-# TODO: open the file, play with the XML, save the file
+# save any unsaved changes before we start messing about with the project file on disk
+$project.Save()
+
+# read in project XML
+$projectXml = [xml](Get-Content $project.FullName)
+$namespace = 'http://schemas.microsoft.com/developer/msbuild/2003'
+
+# remove old imports
+$nodes = @(Select-Xml "//msb:Project/msb:Import[contains(@Project,'\packages\StyleCop.MSBuild.')]" $projectXml -Namespace @{msb = $namespace} | Foreach {$_.Node})
+if ($nodes)
+{
+    foreach ($node in $nodes)
+    {
+        $parentNode = $node.ParentNode
+        [void]$parentNode.RemoveChild($node)
+    }
+}
+
+# work out relateive path to targets
+$absolutePath = [System.IO.PATH]::COMBINE($toolsPath, "StyleCop.targets")
+$absoluteUri = New-Object -typename System.Uri -argumentlist $absolutePath
+$projectUri = New-Object -typename System.Uri -argumentlist $project.FullName
+$relativeUri = $projectUri.MakeRelativeUri($absoluteUri)
+$relativePath = [System.URI]::UnescapeDataString($relativeUri.ToString());
+$relativePath = $relativePath.Replace('/', [System.IO.PATH]::DirectorySeparatorChar);
+
+# add new import
+$import = $projectXml.CreateElement('Import', $namespace)
+$import.SetAttribute('Project', $relativePath)
+$projectXml.Project.AppendChild($import)
+
+# save changes
+$projectXml.Save($project.FullName)
